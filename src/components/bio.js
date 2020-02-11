a934ecdfac17f8407699759fd761d9910101114e
Kevin Chau <kevin@kevinchau.com>
2020-02-10 22:09:34 -0800

    Updated bio.

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,11 +34,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong>, an artist who lives in San
+              Francisco.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Follow him on Twitter
               </a>
             </p>
           </Container>
